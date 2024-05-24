6d4070d9adb71cdcab0c8c13d5d72fe4cc1b6f5e
Altaha <altahabinsyafriadi@gmail.com>
2024-05-24 19:59:40 +0700

    39. Combination Sum.js

---
+++ 39. Combination Sum.js
@@ -0,0 +1,26 @@
+var combinationSum = function (candidates, target) {
+  const result = [];
+  const path = [];
+  candidates.sort((a, b) => a - b);
+  backtrack(candidates, 0, target, path, result);
+  return result;
+};
+
+function backtrack(candidates, start, target, path, result) {
+  if (target === 0) {
+    result.push([...path]);
+    return;
+  }
+  if (target < 0) {
+    return;
+  }
+  for (let i = start; i < candidates.length; i++) {
+    path.push(candidates[i]);
+    backtrack(candidates, i, target - candidates[i], path, result);
+    path.pop();
+  }
+}
+
+let candidates = [2, 3, 1];
+let target = 5;
+console.log(combinationSum(candidates, target));
