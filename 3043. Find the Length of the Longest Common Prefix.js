a8c0c6061a39840bb48f335eddecc5505e57c1b4
altaha056 <altahabinsyafriadi@gmail.com>
2024-09-28 08:43:33 +0700

    3043. Find the Length of the Longest Common Prefix.js

---
+++ 3043. Find the Length of the Longest Common Prefix.js
@@ -0,0 +1,28 @@
+var longestCommonPrefix = function (arr1, arr2) {
+  const prefixMap = new Map();
+  for (let num of arr1) {
+    let strNum = num.toString();
+    let prefix = "";
+    for (let ch of strNum) {
+      prefix += ch;
+      prefixMap.set(prefix, (prefixMap.get(prefix) || 0) + 1);
+    }
+  }
+  let maxLength = 0;
+  for (let num of arr2) {
+    let strNum = num.toString();
+    let prefix = "";
+    for (let ch of strNum) {
+      prefix += ch;
+      if (prefixMap.has(prefix)) {
+        maxLength = Math.max(maxLength, prefix.length);
+      }
+    }
+  }
+  return maxLength;
+};
+
+let arr1 = [1, 10, 100];
+let arr2 = [1000];
+
+console.log(longestCommonPrefix(arr1, arr2));
