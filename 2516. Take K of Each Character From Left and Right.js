f40f855406d4f8af9cebdbf35dd5313e33a4ac0a
altaha056 <altahabinsyafriadi@gmail.com>
2024-11-20 11:25:47 +0700

    2516. Take K of Each Character From Left and Right.js

---
+++ 2516. Take K of Each Character From Left and Right.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var takeCharacters = function (s, k) {
+  let n = s.length;
+  let freq = [0, 0, 0];
+
+  for (let char of s) {
+    freq[char.charCodeAt(0) - "a".charCodeAt(0)]++;
+  }
+
+  if (freq[0] < k || freq[1] < k || freq[2] < k) {
+    return -1;
+  }
+
+  let countOfA = freq[0] - k;
+  let countOfB = freq[1] - k;
+  let countOfC = freq[2] - k;
+
+  let current = [0, 0, 0];
+  let maxLength = 0;
+  let left = 0;
+
+  for (let right = 0; right < n; right++) {
+    current[s[right].charCodeAt(0) - "a".charCodeAt(0)]++;
+
+    while (
+      left <= right &&
+      (current[0] > countOfA || current[1] > countOfB || current[2] > countOfC)
+    ) {
+      current[s[left].charCodeAt(0) - "a".charCodeAt(0)]--;
+      left++;
+    }
+
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return n - maxLength;
+};
+const s = "abc";
+const k = 1;
+console.log(takeCharacters(s, k));
