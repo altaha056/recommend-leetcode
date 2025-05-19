f0b042b0091bfa146cc96a1fd109ee88ce902e43
altaha056 <altahabinsyafriadi@gmail.com>
2025-05-19 19:55:07 +0700

    fix 3024

--- 3024. Type of Triangle.js
+++ 3024. Type of Triangle.js
@@ -1,14 +1,15 @@
 var triangleType = function (nums) {
   if (nums[1] == nums[0] && nums[0] == nums[2]) return "equilateral";
+  if (
+    nums[1] + nums[0] <= nums[2] ||
+    nums[1] + nums[2] <= nums[0] ||
+    nums[0] + nums[2] <= nums[1]
+  )
+    return "none";
+
   if (nums[1] == nums[0] || nums[0] == nums[2] || nums[1] == nums[2])
     return "isosceles";
-  if (
-    nums[1] + nums[0] > nums[2] &&
-    nums[1] + nums[2] > nums[0] &&
-    nums[0] + nums[2] > nums[1]
-  )
-    return "scalene";
-  return "none";
+  return "scalene";
 };
-const nums = [5, 3, 4];
+const nums = [5, 3, 3];
 console.log(triangleType(nums));
