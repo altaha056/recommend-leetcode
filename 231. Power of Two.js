32d5c9d86383eb6fbe62de3351a04d8701dca531
Altaha <64250070+altaha056@users.noreply.github.com>
2025-08-09 13:24:06 +0700

    231. Power of Two.js

--- 231. Power of Two.js
+++ 231. Power of Two.js
@@ -1,10 +1,6 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
 var isPowerOfTwo = function (n) {
   if (n == 1) return true;
-  while (n > 2) n /= 2;
-  return n == 2;
+  if (n < 1) return false;
+  return isPowerOfTwo(n / 2);
 };
-console.log(isPowerOfTwo(8));
+console.log(isPowerOfTwo(16));
