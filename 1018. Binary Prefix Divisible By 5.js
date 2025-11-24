a6bf0e73488f481db01043cd5108331454b79138
Altaha <64250070+altaha056@users.noreply.github.com>
2025-11-24 13:40:38 +0700

    Update 1018. Binary Prefix Divisible By 5.js

--- 1018. Binary Prefix Divisible By 5.js
+++ 1018. Binary Prefix Divisible By 5.js
@@ -2,10 +2,8 @@ var prefixesDivBy5 = function (nums) {
   const ans = [];
   let rem = 0;
   for (const num of nums) {
-    rem = rem * 2 + num;
-    ans.push(rem % 5 == 0);
+    rem = (rem * 2 + num) % 5;
+    ans.push(rem == 0);
   }
   return ans;
 };
-const nums = [0, 1, 1, 1];
-console.log(prefixesDivBy5(nums));
