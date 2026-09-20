4f52235d88f9fcb31aed8fc3ce0d4cb28ed9091c
Altaha <64250070+altaha056@users.noreply.github.com>
2026-09-20 13:30:13 +0700

    Add files via upload

---
+++ 3498. Reverse Degree of a String.js
@@ -0,0 +1,9 @@
+var reverseDegree = function (s) {
+  let ans = 0;
+
+  for (let i = 0; i < s.length; i++)
+    ans += (26 - (s.charCodeAt(i) - 97)) * (i + 1);
+
+  return ans;
+};
+console.log(reverseDegree("abc"));
